1136bdbd3843e78ff238b978787f7f707d2a9770
brandonjp <brandonjp@gmail.com>
2020-07-22 11:40:28 -0500

    version bump v0.1.25

--- add-add-new-to-meta-box-select-advanced.php
+++ add-add-new-to-meta-box-select-advanced.php
@@ -3,28 +3,29 @@
 Plugin Name: Add 'Add New' to Meta Box Select Advanced Fields
 Plugin URI: http://brandonjp.com
 Description: Add 'Add New' to Meta Box Select Advanced Fields
-Version: 0.1
+Version: 0.1.25
 Author: Brandon Pfeiffer
 Author URI: http://brandonjp.com
 
 Text Domain: add-add-new-to-meta-box-select-advanced
 
-With help from: 
-  * https://www.exratione.com/2018/02/the-easiest-javascript-modal-for-administrative-pages-in-wordpress-4/
-  * https://codex.wordpress.org/Javascript_Reference/ThickBox
-  * http://rizqy.me/create-modal-box-on-wordpress-dashboard/
-*/
+With help from:
+ * https://www.exratione.com/2018/02/the-easiest-javascript-modal-for-administrative-pages-in-wordpress-4/
+ * https://codex.wordpress.org/Javascript_Reference/ThickBox
+ * http://rizqy.me/create-modal-box-on-wordpress-dashboard/
+ */
 
-
-function add_add_new_to_mb_admin_enqueue() {
+function add_add_new_to_mb_admin_enqueue()
+{
 
     $screen = get_current_screen();
 
     // load on the NEW and EDIT screens of all post types
-    if ( 'post' === $screen->base ) {
+    if ('post' === $screen->base) {
         add_thickbox();
-        wp_enqueue_script('add_add_new_to_mb_script', plugin_dir_url( __FILE__ ) . 'add_add_new_to_mb.js');
+        wp_enqueue_script('add_add_new_to_mb_script', plugin_dir_url(__FILE__) . 'add_add_new_to_mb.js?v=0.1.25');
     }
 
 }
+
 add_action('admin_enqueue_scripts', 'add_add_new_to_mb_admin_enqueue');
