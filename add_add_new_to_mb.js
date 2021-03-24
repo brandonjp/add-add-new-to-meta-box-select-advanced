6727d5e6877f6cc062daf0bb8d0bb83d4a150521
brandonjp <brandonjp@gmail.com>
2021-03-24 03:35:04 -0500

    v0.1.67 - append btn to parent & noConflict
    rabbit hole of trying to prevent race condition with select2 - switching to appending btn to parent of select element - leaving custom noConflict in place bcs it works

--- add_add_new_to_mb.js
+++ add_add_new_to_mb.js
@@ -1,111 +1,111 @@
-(function($) {
-    'use strict';
-    $(window).load(function() {
-        // console.log('Inside jQuery add_add_new_to_mb.js');
-        // in case the WP site is in a subdirectory, this should return the base - similar to using WP: site_url()
-        let siteURL = window.ajaxurl.split('wp-admin')[0];
-        let aanConfig = {
-            useModal: true,
-            modalParams: '&TB_iframe=true',
-            modalWidth: '600',
-            modalHeight: '550',
-            tagNameParam: '&tag-name=',
-            postUrl: siteURL + 'wp-admin/post-new.php?post_type=',
-            taxoUrl: siteURL + 'wp-admin/edit-tags.php?taxonomy=',
-            linkText: 'Add New',
-            linkClass: 'button button-small add-add-new-button cardinalpm-button',
-            linkStyle: ''
-        }
-        jQuery('div.rwmb-field[class*="add-add-new"]').each(function() {
-            let $thisSelect = jQuery(this).find('select.rwmb-select_advanced');
-            let useModal = jQuery(this).hasClass('add-add-new-modal') || aanConfig.useModal;
-            let useWindow = jQuery(this).hasClass('add-add-new-window');
-            if (!!useWindow) useModal = false;
-            let options = $thisSelect.data('options');
-            // only continue if the field is select2 and has options
-            if (options) {
-                // console.warn(options);
-                let type = $thisSelect.data('type');
-                let post_type = $thisSelect.data('post_type');
-                // should be 'taxonomy' or 'taxonomy_advanced' or 'post'
-                if (!type) {
-                  if (options['ajax_data']) {
+aanjq = jQuery.noConflict();
+aanjq(function($$) {
+    // console.log('Inside jQuery add_add_new_to_mb.js');
+    // in case the WP site is in a subdirectory, this should return the base - similar to using WP: site_url()
+    let siteURL = window.ajaxurl.split('wp-admin')[0];
+    let aanConfig = {
+        useModal: true,
+        modalParams: '&TB_iframe=true',
+        modalWidth: '600',
+        modalHeight: '550',
+        tagNameParam: '&tag-name=',
+        postUrl: siteURL + 'wp-admin/post-new.php?post_type=',
+        taxoUrl: siteURL + 'wp-admin/edit-tags.php?taxonomy=',
+        linkText: 'Add New',
+        linkClass: 'button button-small add-add-new-button cardinalpm-button',
+        linkStyle: ''
+    }
+    jQuery('div.rwmb-field[class*="add-add-new"]').each(function() {
+        let $thisSelect = jQuery(this).find('select.rwmb-select_advanced');
+        let useModal = jQuery(this).hasClass('add-add-new-modal') || aanConfig.useModal;
+        let useWindow = jQuery(this).hasClass('add-add-new-window');
+        if (!!useWindow) useModal = false;
+        let options = $thisSelect.data('options');
+        // only continue if the field is select2 and has options
+        if (options) {
+            // console.warn(options);
+            let type = $thisSelect.data('type');
+            let post_type = $thisSelect.data('post_type');
+            // should be 'taxonomy' or 'taxonomy_advanced' or 'post'
+            if (!type) {
+                if (options['ajax_data']) {
                     // when ajax => true on metabox field settings...
                     type = options['ajax_data']['field']['type'];
-                  } else {                  
+                } else {
                     // when ajax => false on metabox field settings...
                     type = $thisSelect.hasClass('rwmb-taxonomy') ? 'taxonomy' : 'post';
-                  }
                 }
-                if (!post_type) {
-                  if (options['ajax_data']) {
+            }
+            if (!post_type) {
+                if (options['ajax_data']) {
                     let query_args = options['ajax_data']['field']['query_args'];
                     // should be the post/tax type name: foreman, project-manager, contact, customer, etc
                     post_type = (type === 'post') ? query_args['post_type'] : query_args['taxonomy'][0];
-                  }
                 }
-                // console.warn(useModal, options, type, query_args, post_type);
-                let linkText = aanConfig.linkText ? aanConfig.linkText : 'Add New';
-                let linkStyle = aanConfig.linkStyle ? aanConfig.linkStyle : ' ';
-                let hrefID = `${post_type}_add_new_btn`;
-                let classAttr = 'class="' + (useModal ? ' thickbox ' : ' ') + aanConfig.linkClass + '"';
-                let targetAttr = !useModal ? 'target="_blank"' : ' ';
-                let modalParams = aanConfig.modalParams + '&width=' + aanConfig.modalWidth + '&height=' + aanConfig.modalHeight;
-                let baseUrl = (type === 'post') ? aanConfig.postUrl : aanConfig.taxoUrl;
-                let addNewUrl = baseUrl + post_type + aanConfig.tagNameParam + '0' + (useModal ? modalParams : '');
-                let $hrefElem = jQuery(`<a href="${addNewUrl}" ${targetAttr} ${classAttr} id="${hrefID}" style="${linkStyle}">${linkText}</a>`);
-                $hrefElem.on('click', function(e) {
-                    let $thisBtn = jQuery(this);
-                    if (window.__CardinalTempDoNormalClick) {
-                        window.__CardinalTempDoNormalClick = false;
-                        let tb_unload_count = 1;
-                        let showSelect2onClose = function showSelect2onClose() {
-                            if (tb_unload_count > 1) {
-                                tb_unload_count = 1;
-                            } else {
-                                jQuery(window).unbind('tb_unload', showSelect2onClose);
-                                tb_unload_count = tb_unload_count + 1;
-                                $thisBtn.attr('href').replace(window.__CardinalTempNewTagName, '0');
-                                $thisSelect.attr('disabled',false).prop('disabled',false);
-                                $thisSelect.trigger('focus');
-                                $thisSelect.select2('open');
-                                window.__CardinalTempNewTagName = '';
-                            }
-                        };
-                        jQuery(window).bind('tb_unload', showSelect2onClose);
-                    } else {
-                        e.preventDefault();
-                        window.__CardinalTempDoNormalClick = true;
-                        $thisSelect.attr('disabled',true).prop('disabled',true);
-                        let zroTagNameParam = aanConfig.tagNameParam + '0';
-                        let newTagNameParam = aanConfig.tagNameParam + (window.__CardinalTempNewTagName.length ? window.__CardinalTempNewTagName : '0');
-                        let newHref = $thisBtn.attr('href').replace(zroTagNameParam, newTagNameParam);
-                        // let zroHref = $thisBtn.attr('href').replace(newTagNameParam, zroTagNameParam);
-                        $thisBtn.attr('href', newHref).trigger('click');
-                        window.doubleTBcheck = setInterval(removeDoubleTB_title,100);
-                    }
-                    // let dataHref = jQuery(this).data('href');
-                });
-                $thisSelect.next().after($hrefElem);
             }
-        });
-        window.__CardinalTempDoNormalClick = false;
-        window.__CardinalTempNewTagName = '';
-        jQuery('.select2.select2-container, input.select2-search__field').on('focus click', function() {
-            window.__CardinalTempNewTagName = '';
-        });
-        jQuery('body').on('keyup', 'input.select2-search__field', function() {
-            let tagName = jQuery(this).val();
-            window.__CardinalTempNewTagName = tagName || '';
-        });
-        function removeDoubleTB_title() {
-            // if there's a double iframe header/close bar, remove it
-            let $TB_title = jQuery('div#TB_title');
-            if ($TB_title.length > 1) {
-                $TB_title.first().addClass('first');
-                $TB_title.not('.first').remove();
-            } 
-            if ($TB_title.length === 1) clearInterval(window.doubleTBcheck);
+            // console.warn(useModal, options, type, post_type);
+            let linkText = aanConfig.linkText ? aanConfig.linkText : 'Add New';
+            let linkStyle = aanConfig.linkStyle ? aanConfig.linkStyle : ' ';
+            let hrefID = `${post_type}_add_new_btn`;
+            let classAttr = 'class="' + (useModal ? ' thickbox ' : ' ') + aanConfig.linkClass + '"';
+            let targetAttr = !useModal ? 'target="_blank"' : ' ';
+            let modalParams = aanConfig.modalParams + '&width=' + aanConfig.modalWidth + '&height=' + aanConfig.modalHeight;
+            let baseUrl = (type === 'post') ? aanConfig.postUrl : aanConfig.taxoUrl;
+            let addNewUrl = baseUrl + post_type + aanConfig.tagNameParam + '0' + (useModal ? modalParams : '');
+            let $hrefElem = jQuery(`<a href="${addNewUrl}" ${targetAttr} ${classAttr} id="${hrefID}" style="${linkStyle}">${linkText}</a>`);
+            $hrefElem.on('click', function(e) {
+                let $thisBtn = jQuery(this);
+                if (window.__CardinalTempDoNormalClick) {
+                    window.__CardinalTempDoNormalClick = false;
+                    let tb_unload_count = 1;
+                    let showSelect2onClose = function showSelect2onClose() {
+                        if (tb_unload_count > 1) {
+                            tb_unload_count = 1;
+                        } else {
+                            jQuery(window).unbind('tb_unload', showSelect2onClose);
+                            tb_unload_count = tb_unload_count + 1;
+                            $thisBtn.attr('href').replace(window.__CardinalTempNewTagName, '0');
+                            $thisSelect.attr('disabled', false).prop('disabled', false);
+                            $thisSelect.trigger('focus');
+                            $thisSelect.select2('open');
+                            window.__CardinalTempNewTagName = '';
+                        }
+                    };
+                    jQuery(window).bind('tb_unload', showSelect2onClose);
+                } else {
+                    e.preventDefault();
+                    window.__CardinalTempDoNormalClick = true;
+                    $thisSelect.attr('disabled', true).prop('disabled', true);
+                    let zroTagNameParam = aanConfig.tagNameParam + '0';
+                    let newTagNameParam = aanConfig.tagNameParam + (window.__CardinalTempNewTagName.length ? window.__CardinalTempNewTagName : '0');
+                    let newHref = $thisBtn.attr('href').replace(zroTagNameParam, newTagNameParam);
+                    // let zroHref = $thisBtn.attr('href').replace(newTagNameParam, zroTagNameParam);
+                    $thisBtn.attr('href', newHref).trigger('click');
+                    window.doubleTBcheck = setInterval(removeDoubleTB_title, 100);
+                }
+                // let dataHref = jQuery(this).data('href');
+            });
+            // $thisSelect.next().after($hrefElem);
+            $thisSelect.parent().append($hrefElem);
         }
     });
-})(jQuery);
+    window.__CardinalTempDoNormalClick = false;
+    window.__CardinalTempNewTagName = '';
+    jQuery('.select2.select2-container, input.select2-search__field').on('focus click', function() {
+        window.__CardinalTempNewTagName = '';
+    });
+    jQuery('body').on('keyup', 'input.select2-search__field', function() {
+        let tagName = jQuery(this).val();
+        window.__CardinalTempNewTagName = tagName || '';
+    });
+
+    function removeDoubleTB_title() {
+        // if there's a double iframe header/close bar, remove it
+        let $TB_title = jQuery('div#TB_title');
+        if ($TB_title.length > 1) {
+            $TB_title.first().addClass('first');
+            $TB_title.not('.first').remove();
+        }
+        if ($TB_title.length === 1) clearInterval(window.doubleTBcheck);
+    }
+});
